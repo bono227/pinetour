6fcebd58c44699b8afc881348dc32e48342d4bae
bono227 <dwctab@gmail.com>
2024-10-03 01:03:43 -0500

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { HeaderLogo } from "../assets/HeaderLogo";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div id="header">
+      <HeaderLogo />
+    </div>
+  );
+};
---
+++ src/components/trip/Trip.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Trip.css";
+
+export const Trip = () => {
+  return (
+    <div id="destination-container">
+      <h1>Khuvsgul Lake</h1>
+      <h4>TRIP</h4>
+      <div>
+        <img src="images/image1.png" />
+      </div>
+    </div>
+  );
+};
